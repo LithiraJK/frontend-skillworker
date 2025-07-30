5d51b341cacaae96a9286c32bd5182202eca9216
LithiraJK <kariyawasamlithira2@gmail.com>
2025-07-31 00:35:05 +0530

    Login & Registration UI Implemented

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
     console.log("Document is ready!");
+
+    $('#getStartedBtn').on('click', function() {
+        console.log("Get Started button clicked!");
+        window.location.href = 'pages/loginPage.html';
+    });
     
 });
---
+++ js/signupFirstPage.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+    let selectedType = null;
+
+    $('.user-type-toggle').on('click', function () {
+        // Remove active class from all cards
+        $('.user-type-toggle').removeClass('active');
+
+        // Add active class to clicked card
+        $(this).addClass('active');
+
+        // Store selected type
+        selectedType = $(this).data('type');
+
+        // Enable continue button
+        $('#continueBtn').prop('disabled', false);
+
+        // Update button text based on selection
+        if (selectedType === 'freelancer') {
+            $('#continueBtn').text('Join as a Freelancer');
+        } else {
+            $('#continueBtn').text('Join as a Client');
+        }
+    });
+
+    // Handle continue button click
+    $('#continueBtn').on('click', function () {
+        if (selectedType === 'freelancer') {
+            window.location.href = '../pages/signupSecondPage.html?role=freelancer';
+        } else if (selectedType === 'client') {
+            window.location.href = '../pages/signupSecondPage.html?role=client';
+        }
+    });
+});
