3c1092db72c6a35e3e587e2f5c92682ef687b578
LithiraJK <kariyawasamlithira2@gmail.com>
2025-09-20 14:07:00 +0530

    subscription feature modified

--- js/main.js
+++ js/main.js
@@ -1,9 +1,34 @@
-$(document).ready(function() {
+$(document).ready(function () {
     console.log("Document is ready!");
 
-    $('#getStartedBtn').on('click', function() {
+    $('#getStartedBtn').on('click', function () {
         console.log("Get Started button clicked!");
         window.location.href = 'pages/login-page.html';
     });
-    
+
+    $('#searchForm').on('submit', function (e) {
+        e.preventDefault();
+        const searchQuery = $('#searchQuery').val();
+        const location = $('#location').val();
+        console.log('Searching for:', searchQuery, 'in', location);
+    });
+
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        const target = $(this.getAttribute('href'));
+        if (target.length) {
+            $('html, body').animate({
+                scrollTop: target.offset().top - 80
+            }, 1000);
+        }
+    });
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 50) {
+            $('.navbar-custom').addClass('shadow-sm');
+        } else {
+            $('.navbar-custom').removeClass('shadow-sm');
+        }
+    });
+
 });
